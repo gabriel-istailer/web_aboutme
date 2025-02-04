eb1ec9a4ea7da1cc64fd0e13025288ddb71169a0
Gabriel Istailer <gabriel.istailer@hotmail.com>
2025-02-04 13:03:35 -0300

    Estilizei mais o formulário de cadastro

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -12,6 +12,7 @@ export default function Register() {
 
     const [isRegistered, setIsRegistered] = useState(false);
     const [isFirstRender, setIsFirstRender] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
 
     useEffect(() => {
         const registeredStatus = searchParams.get('isRegistered') === 'yes';
@@ -33,6 +34,10 @@ export default function Register() {
         setIsRegistered((prev) => !prev);
     }
 
+    function changeShowPassword() {
+        setShowPassword((prev) => !prev);
+    }
+
     return (
         <div className="register">
 
@@ -42,7 +47,7 @@ export default function Register() {
                     <Link className="title font-LilyScriptOne" href='/'>AboutMe</Link>
                 </nav>
 
-                <section className="register-header-section flex-h-center">
+                <section className="register-header-section flex-center">
                     <div className='background flex-v-center overflow-hidden'>
 
                         <form className="form signup flex-center flex-column" id='formSignUp'>
@@ -51,23 +56,23 @@ export default function Register() {
 
                                 <h1 className="title title-signup text-center">Cadastro</h1>
 
-                                <label htmlFor="name-input-signup" className='label'>Nome:</label>
-                                <div className="label-container-signup label-name-signup">
-                                    <input type="text" className='input input-name-signup' name="name-input-signup" id="nameInputSignUp" maxLength={20} minLength={2} required />
-                                </div>
+                                <label htmlFor="nameInputSignUp" className='label'>Nome:</label>
+                                <input type="text" className='input input-name-signup' name="name-input-signup" id="nameInputSignUp" maxLength={20} minLength={2} required />
 
-                                <label htmlFor="email-input-signup" className='label'>Email:</label>
+                                <label htmlFor="emailInputSignUp" className='label'>Email:</label>
                                 <input type="email" className='input input-email-signup' name="email-input-signup" id="emailInputSignUp" required />
 
-                                <label htmlFor="password-input-signup" className='label'>Senha:</label>
-                                <input type="password" className='input input-password-signup' name="password-input-signup" id="passwordInputSignUp" required />
+                                <label htmlFor="passwordInputSignUp" className='label'>Senha:</label>
+                                <input type={showPassword ? 'text' : 'password'} className='input input-password-signup' name="password-input-signup" id="passwordInputSignUp" required />
 
-                                <label htmlFor="show-password-input-signup">Mostrar senha:</label>
-                                <input type="checkbox" name="show-password-input-signup" id="showPasswordInputSignUp" />
+                                <div className="label-container-checkbox-signup flex-v-center">
+                                    <input type="checkbox" checked={showPassword} onChange={changeShowPassword} className='checkbox-show-password-signup' name="checkbox-show-password-signup" id="showPasswordCheckboxSignUp" />
+                                    <label htmlFor="showPasswordCheckboxSignUp" className='label-checkbox-signup'>Mostrar senha:</label>
+                                </div>
 
-                                <button type="button">Cadastrar</button>
+                                <button type="button" className='button-signup'>Cadastrar</button>
 
-                                <button type="button" onClick={changeRegisteredStatus}>Entrar</button>
+                                <button type="button" onClick={changeRegisteredStatus} className='button-go-signin'>Já tem uma conta cadastrada? Então entre por aqui.</button>
 
                             </div>
 
