0bb03b1341d8dac6b8672df99e17000a03b33df0
Gabriel Istailer <gabriel.istailer@hotmail.com>
2025-02-12 13:46:31 -0300

    Configurei mais um pouco o endpoint da verificação de email

--- src/app/api/send-email-verification/route.js
+++ src/app/api/send-email-verification/route.js
@@ -1,2 +1,13 @@
 import { NextResponse } from "next/server";
 import database from "@/lib/database";
+import emailVerification from "@/lib/nodemailer";
+
+export async function POST(req) {
+    try {
+        const body = await req.json();
+        await emailVerification.sendEmailSignUp(body.email);
+    } catch (error) {
+        console.log('Erro no POST da API Route send-email-verification: ', error);
+        return NextResponse.json({error: 'Erro no POST da API Route do servidor'}, {status: 500});
+    }
+}
