02181199aa210dbb268171a2ba4d14fb0498f528
Gabriel Istailer <gabriel.istailer@hotmail.com>
2025-05-07 18:58:44 -0300

    Iniciei o sistema de recuperação de senha

---
+++ src/app/api/users/signin/recover-password/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import NodemailerController from "@/lib/controllers/NodemailerController";
+import jwt from 'jsonwebtoken';
+
+export async function GET(req) {
+    try {
+        const { searchParams } = new URL(req.url);
+
+        const email = searchParams.get('email');
+
+        const token = jwt.sign(email, process.env.JWT_SECRET);
+
+        await NodemailerController.sendPasswordRecovery(token, email);
+
+        return NextResponse.json({ message: 'Password recovery sent to user by email' }, { status: 200 });
+
+    } catch (error) {
+        console.log('Erro in API Route users/signin/recover-password: ', error);
+        return NextResponse.json({ message: 'Server Route API Error' }, { status: 500 });
+    }
+}
